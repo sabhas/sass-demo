22fd3afb038b3dfa59992699fe19c10f115a0af1
sabhas <sabirbk06@gmail.com>
2021-08-29 15:55:20 +0500

    move scss files into separate folder

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles() {
-  return src('*.scss').pipe(sass()).pipe(dest('css'))
+  return src('myStyle/**/*.scss').pipe(sass()).pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['*.scss'], buildStyles)
+  watch(['myStyle/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
