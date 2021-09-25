029ba5b8e6ddf7678cabb435ae576d1a6a2dcac3
sabhas <sabirbk06@gmail.com>
2021-09-25 22:02:46 +0500

    use gulp-purgecss package, it will only compile those css classes which are actually being used in html files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
-
+const purgecss = require('gulp-purgecss')
 function buildStyles() {
-  return src('myStyle/**/*.scss').pipe(sass()).pipe(dest('css'))
+  return src('myStyle/**/*.scss')
+    .pipe(sass())
+    .pipe(purgecss({ content: ['*.html'] }))
+    .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['myStyle/**/*.scss'], buildStyles)
+  watch(['myStyle/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
